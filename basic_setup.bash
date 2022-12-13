003b046f78c25f1b5a4d490d1922dc615987455f
Jiachao Deng <jcd06@qq.com>
2022-12-12 21:10:21 -0800

    update.

--- basic_setup.bash
+++ basic_setup.bash
@@ -18,18 +18,19 @@ $1=="#PubkeyAuthentication" {$1="PubkeyAuthentication"}
 rm -f $file.old
 
 dnf install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Change default shell to zsh.
-usermod -s $(which zsh) $(whoami)
+echo "Get oh-my-zsh."
+echo "sh -c "\$\(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh\)""
 
-script_dir=$(dirname `which $0`)
+echo "Get ZSH plugins."
+echo "git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+echo "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions $script_dir/../.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $script_dir/../.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+echo "Copy .zshrc."
+echo "cp ~/fedora_setup/.zshrc ~/"
 
-rm $script_dir/../.zshrc
-cp $script_dir/.zshrc $script_dir/../
+echo "Change default shell to zsh using the following command."
+echo "usermod -s $(which zsh) $(whoami)"
 
 echo "***********************************************************"
 echo "Need to add .pub to authorized_keys and set permits to 644."
