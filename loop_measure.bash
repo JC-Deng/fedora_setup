15eeae58a599be0dea8f731d8f907e1afcce08ee
Jiachao Deng <jcd06@qq.com>
2022-12-13 01:52:57 -0800

    updated

--- loop_measure.bash
+++ loop_measure.bash
@@ -2,9 +2,9 @@ SCRIPT_PATH="$(dirname -- "$BASH_SOURCE")"
 > $SCRIPT_PATH/build.log
 
 while :; do
-    df -h >> $SCRIPT_PATH/build.log
-    echo "*****************************************" >> $SCRIPT_PATH/build.log
-    free -h >> $SCRIPT_PATH/build.log
-    echo "*****************************************" >> $SCRIPT_PATH/build.log
-    sleep 15
+    free -h | tail -n 2 >> $SCRIPT_PATH/build.log
+    du -hs /home/d/install >> $SCRIPT_PATH/build.log
+    du -hs /home/d/tmptmp >> $SCRIPT_PATH/build.log
+    echo "*******************************************************************" >> $SCRIPT_PATH/build.log
+    sleep 5
 done
