b887b27bd180e5a68404ca2aec148be0d06b1ecb
Jiachao Deng <jcd06@qq.com>
2022-12-13 01:44:52 -0800

    added measure

---
+++ loop_measure.bash
@@ -0,0 +1,10 @@
+SCRIPT_PATH="$(dirname -- "$BASH_SOURCE")"
+> $SCRIPT_PATH/build.log
+
+while :; do
+    df -h >> $SCRIPT_PATH/build.log
+    echo "*****************************************" >> $SCRIPT_PATH/build.log
+    free -h >> $SCRIPT_PATH/build.log
+    echo "*****************************************" >> $SCRIPT_PATH/build.log
+    sleep 5
+done
