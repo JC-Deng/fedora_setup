e6f3a154bc5bb3a0516f8ce9a6894f6b66abb5a9
Jiachao Deng <jcd06@qq.com>
2022-12-12 21:12:40 -0800

    update.

--- basic_setup.bash
+++ basic_setup.bash
@@ -19,18 +19,26 @@ rm -f $file.old
 
 dnf install zsh -y
 
+echo "***********************************************************"
 echo "Get oh-my-zsh."
 echo "sh -c "\$\(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh\)""
 
+echo "***********************************************************"
 echo "Get ZSH plugins."
 echo "git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 echo "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 
+echo "***********************************************************"
 echo "Copy .zshrc."
 echo "cp ~/fedora_setup/.zshrc ~/"
 
+echo "***********************************************************"
 echo "Change default shell to zsh using the following command."
 echo "usermod -s $(which zsh) $(whoami)"
 
 echo "***********************************************************"
 echo "Need to add .pub to authorized_keys and set permits to 644."
+
+echo "***********************************************************"
+echo "git config --global user.email \"jcd06@qq.com\""
+echo "git config --global user.name \"Jiachao Deng\""
